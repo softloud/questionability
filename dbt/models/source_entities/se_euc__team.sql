e46e5e7f67165ae1f2e989aabc2ef1e34f6e3d92
Charles T. Gray <charlestigray@gmail.com>
2026-05-05 10:27:31 +1000

    #112 hack to remove dupes

--- dbt/models/source_entities/se_euc__team.sql
+++ dbt/models/source_entities/se_euc__team.sql
@@ -1,3 +1,12 @@
-select *
+with counted as (
+select 
+  *, 
+  -- hack to remove duplicates until we can fix the source data
+  row_number() over (partition by team_id order by team_id) as row_num
 from {{ ref('euc_team') }}
+)
+select *
+from counted
 where team_id is not null
+-- hack to remove duplicates until we can fix the source data
+and row_num = 1
