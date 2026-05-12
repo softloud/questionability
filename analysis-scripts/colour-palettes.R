33f7836a72bb2cd32ba36f1842c5af7666ee7742
Hannah Fraser <hfraser@unimelb.edu.au>
2026-05-12 14:08:55 +1000

    #168 fixed the readme pallette instructions took pallette out of treemap and put into the pallet R file

---
+++ analysis-scripts/colour-palettes.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+# create conclusions colour palette
+conclusion_palette <- c(
+  "negative" = "#D95F5F",
+  "positive" = "#4E9E6E",
+  "mixed" = "#D4884A",
+  "none" = "#8B95A3",
+  "conclusion missing" = "#B8A8C8"
+)
+
